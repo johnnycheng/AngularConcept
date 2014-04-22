f3c169e2852b7bea74858708425ba41ff96698cd
Johnny Cheng <johnny.cheng@zenithoptimedia.com>
2014-04-22 17:37:49 +0100

    bringing the project upto date with the lastest angular generator

--- src/app/app.js
+++ src/app/app.js
@@ -2,24 +2,28 @@
 
 angular.module('app', ['user-filters'])
     .config(function($routeProvider, $locationProvider){
-
         $locationProvider.html5Mode(true);
-
-        $routeProvider.when('/',
+        $routeProvider
+            .when('/',
             {
                 templateUrl: 'app/main.html',
                 controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
             })
     });
 
 angular.module('user-filters', ['ngRoute'])
     .config(function($routeProvider, $locationProvider){
-
         $locationProvider.html5Mode(true);
-
-        $routeProvider.when('/user-filters',
+        $routeProvider
+            .when('/user-filters',
             {
                 templateUrl: 'app/user-filters/list.html',
                 controller: 'UserFiltersCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
             })
     });
