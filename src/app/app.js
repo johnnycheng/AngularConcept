796e4de643e9838e1f121852fcf13e118b84bad0
Johnny Cheng <johnny.cheng@zenithoptimedia.com>
2014-04-22 13:04:33 +0100

    added angular-route and updated angular version

--- src/app/app.js
+++ src/app/app.js
@@ -2,4 +2,13 @@
 
 angular.module('app', ['user-filters']);
 
-angular.module('user-filters', []);
+angular.module('user-filters', ['ngRoute'])
+    .config(function($routeProvider, $locationProvider){
+
+        $locationProvider.html5Mode(true);
+
+        $routeProvider.when('/user-filters',
+            {
+                templateUrl: 'app/user-filters/list.html'
+            })
+    });
