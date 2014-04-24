b2fdd8b19b944fa5d5abab2cf7082208c96a73fb
Johnny Cheng <johnny.cheng@zenithoptimedia.com>
2014-04-24 12:11:56 +0100

    removing unnecessary mvc code

--- src/Global.asax.cs
+++ src/Global.asax.cs
@@ -1,27 +1,20 @@
 ﻿using System;
-using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 using System.Web.Routing;
 using Newtonsoft.Json.Serialization;
 using Sentry.Common.Web.Utilities;
 
 namespace AngularConcept
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-    public class AngularConcept : System.Web.HttpApplication
+    public class AngularConcept : HttpApplication
     {
         protected void Application_Start()
         {
             var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
             json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
-            AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
 
